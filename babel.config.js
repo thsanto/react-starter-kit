406e070a39ba0a2cad51a829fa1d544f4196db65
Sang Au <auvansang@outlook.com>
2019-08-14 21:54:54 +0700

    Update test config

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,6 @@ module.exports = function(api) {
     [
       '@babel/preset-env',
       {
-        modules: 'commonjs',
         targets: {
           browsers: isDevEnv
             ? ['last 1 chrome version', 'last 1 firefox version', 'last 1 safari version']
--- jest.config.js
+++ jest.config.js
@@ -3,21 +3,22 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
   coverageReporters: ['html'],
   // setupFiles: ['react-app-polyfill/jsdom'],
-  setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
     '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
   ],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
-    // '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
-    // '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js'
-  },
+  // transform: {
+  //   '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  //   '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+  //   '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js'
+  // },
   transformIgnorePatterns: [
     '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
     '^.+\\.module\\.(css|sass|scss)$'
   ],
-  modulePaths: ['node_modules', 'src'],
+  // modulePaths: [],
+  moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     '^react-native$': 'react-native-web',
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy'
